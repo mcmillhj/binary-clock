f405cbb211672c4e0416eb3429ee28ca8cf24a4d
Hunter McMillen <mcmillhj@gmail.com>
2016-02-17 22:07:51 -0500

    initial commit
    A simple binary clock that refreshes once a second. The set bits are represented by white 1's and the unset bits are represented by cyan 0's

---
+++ binary-clock.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Term::ANSIColor qw(:constants);
+use Term::Cap;
+local $Term::ANSIColor::AUTORESET = 1;
+
+sub get_time {
+   my ($second, $minute, $hour) = localtime(time);
+
+   my @hours   = (int $hour   / 10, int $hour   % 10);
+   my @minutes = (int $minute / 10, int $minute % 10);
+   my @seconds = (int $second / 10, int $second % 10);
+   return 
+         $hours[0]   << 20 
+       | $hours[1]   << 16 
+       | $minutes[0] << 12
+       | $minutes[1] << 8
+       | $seconds[0] << 4
+       | $seconds[1];
+}
+
+sub draw {
+    my $time = get_time();
+
+    foreach my $i ( 20, 21, 22, 23 ) {
+        my $j = $i;
+        while ( $j >= 0 ) {
+            (($time >> $j) & 1) == 1
+                ? print WHITE "1 "
+                : print  CYAN "0 ";
+            $j -= 4;
+        }
+        print "\n";
+    }
+}
+
+{
+    my $terminal = Term::Cap->Tgetent( { OSPEED => 9600 } );
+    my $clear_screen = $terminal->Tputs('cl');
+        
+    sub clear_screen {
+        print $clear_screen;
+    }
+}
+
+while ( 1 ) {
+    draw();
+    sleep 1;
+    clear_screen();
+}
+
+END {
+    print RESET;
+}
